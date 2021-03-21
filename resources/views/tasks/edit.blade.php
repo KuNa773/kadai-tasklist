254b7d3d15e6aba8f3e4c74301c7ad9f7ca45f38
nanami.kuramochi <kuranana1114@gmail.com>
2021-03-21 12:31:39 +0000

    fix

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -11,8 +11,10 @@
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                   {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
                 </div>
 
                 {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
